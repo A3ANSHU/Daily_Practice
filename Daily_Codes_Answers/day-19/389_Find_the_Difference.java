3725ddc29a060cc0f519f53772cf4f3dadb71ced
A3ANSHU <abhinandanambekar@gmail.com>
2024-01-15 23:51:45 +0530

    day-19

--- Daily_Codes_Answers/day-19/389_Find_the_Difference.java
+++ Daily_Codes_Answers/day-19/389_Find_the_Difference.java
@@ -1,9 +1,28 @@
+import java.util.Arrays;
+
 class Anshu {
     static char findTheDifference(String s, String t) {
-       
-        return 'a';
+        char s1[] = s.toCharArray();
+        char t1[] = t.toCharArray();
+        Arrays.sort(s1);
+        Arrays.sort(t1);
+        for(int i=0 ; i<s1.length ; i++){
+            if(s1[i]!=t1[i]){
+                return t1[i];
+            }
+        }
+        return t1[t1.length-1];
     }
     public static void main(String[] args) {
-        
+
+        String s1 = "abcd";
+        String t1 = "abcde";
+        String s2 = "";
+        String t2 = "y";
+
+        System.out.println("-- test case 1 --");
+        System.out.println("Input : s - "+s1+" t - "+t1+"\nOutput : "+findTheDifference(s1,t1));
+        System.out.println("-- test case 2 --");
+        System.out.println("Input : s - "+s2+" t - "+t2+"\nOutput : "+findTheDifference(s2,t2));
     }
 }
---
+++ Daily_Codes_Answers/day-19/389_Find_the_Difference.py
@@ -0,0 +1,22 @@
+def findTheDifference(s,t):
+    s1 = list(s)
+    t1 = list(t)
+    
+    s1.sort()
+    t1.sort()
+    
+    for i in range(len(s1)):
+        if(s1[i]!=t1[i]):
+            return t1[i]
+    return t1[len(t1)-1]
+           
+    
+s1 = "abcd"
+t1 = "abcde"
+s2 = ""
+t2 = "y"
+
+print("-- test case 1 --")
+print(f"Input : s - {s1} t - {t1}\nOutput : {findTheDifference(s1,t1)}")
+print("-- test case 2 --")
+print(f"Input : s - {s2} t - {t2}\nOutput : {findTheDifference(s2,t2)}")
