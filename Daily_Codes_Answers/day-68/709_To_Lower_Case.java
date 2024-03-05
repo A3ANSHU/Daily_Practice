9f30b156b572bfc39ae66ed5a3ed57885f625a2c
A3ANSHU <abhinandanambekar@gmail.com>
2024-03-05 23:30:25 +0530

    day-68

---
+++ Daily_Codes_Answers/day-68/709_To_Lower_Case.java
@@ -0,0 +1,8 @@
+class Solution {
+    static String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+    public static void main(String[] args) {
+        System.out.println(toLowerCase("Hello"));
+    }
+}
