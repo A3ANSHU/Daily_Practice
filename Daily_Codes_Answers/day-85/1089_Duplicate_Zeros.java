2f3d5bb3b22e75f5a01b646aa8c36f695e2d499b
A3ANSHU <abhinandanambekar@gmail.com>
2024-03-24 01:44:22 +0530

    day-85

--- Daily_Codes_Answers/day-85/1089_Duplicate_Zeros.java
+++ Daily_Codes_Answers/day-85/1089_Duplicate_Zeros.java
@@ -1,17 +1,19 @@
+import java.util.Arrays;
+
 class Solution {
     static void duplicateZeros(int[] arr) {
-        int n1=arr[0];
-        int n2=arr[0];
+        StringBuffer str = new StringBuffer();
         for(int i=0 ; i<arr.length ; i++){
-            if(arr[i]==0 && i!=arr.length-1){
-                n1=n2;
-                n2=arr[i];
-            }else{
-                arr[i]=n1;
+            str.append(arr[i]+"");
+            if(arr[i]==0){
+                str.append("0");
             }
+        }
+        for(int i=0 ; i<arr.length ; i++){
+            arr[i]= str.charAt(i)-'0';
         }
     }
     public static void main(String[] args) {
-        duplicateZeros(new int[]{1,0,2,3,0,4,5,0});
+        duplicateZeros(new int[]{1,0,0,2,3,0,4,5,0});
     }
 }
